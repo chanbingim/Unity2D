b2972e0f33947391a681b6a0db1a0c363fceaf39
chanbingim <127704542+chanbingim@users.noreply.github.com>
2026-03-31 11:45:48 +0900

    프라우드넷 초기세팅

---
+++ Project_ZZZ/Assets/Server/Client/GameClient.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Nettention.Proud;
+
+public class GameClient : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        NetClient netClient = new NetClient();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
